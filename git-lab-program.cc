0ce6f2fcddb72fca90377cc69c2a1a84f982c9d6
Logan Davis <ld436822@ohio.edu>
2023-09-05 17:51:48 -0400

    done

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+*   File: git-lab-program.cc
+*   Author: Logan Davis
+*   Date: 09/05/2023
+*   Description: Creating a C++ Program in git-lab2
+*/
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    /*add code*/
+    return 0;
+    cout << "Hello Git!!" << endl;
+}//main
